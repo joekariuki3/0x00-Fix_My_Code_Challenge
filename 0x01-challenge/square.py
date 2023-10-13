8544f529ba33a912845ccb020b18a82b9a0f5795
joekariuki3 <joekariuki3@gmail.com>
2023-10-13 12:24:26 +0300

    added documentation and fixed some pycode style in square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 
 class square():
-    
+    """
+    class representing a square
+    having class artributes of width
+    and height
+    """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """
+        declaration of the square
+        class
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +22,19 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """
+        method to calculate the perimeter
+        of the square
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        method to retuen string rep
+        of the square
+        """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
